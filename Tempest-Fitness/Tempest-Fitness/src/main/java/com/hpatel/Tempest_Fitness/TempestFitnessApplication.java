02bab5c0e61d246a71082eaa5164f3ecc061b88f
Harsh <hpatel27@ncsu.edu>
2024-12-17 21:11:28 -0500

    The application runner file

---
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/TempestFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.hpatel.Tempest_Fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempestFitnessApplication.class, args);
+	}
+
+}
---
+++ Tempest-Fitness/Tempest-Fitness/src/test/java/com/hpatel/Tempest_Fitness/TempestFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpatel.Tempest_Fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempestFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
