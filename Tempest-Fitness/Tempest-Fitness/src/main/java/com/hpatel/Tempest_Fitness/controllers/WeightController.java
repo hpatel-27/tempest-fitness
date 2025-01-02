aa62006834b6ab583e27d4c1134dd96f6f17efa8
Harsh <hpatel27@ncsu.edu>
2025-01-02 00:47:43 -0500

    Updated some of the comments to be more correct

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/controllers/WeightController.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/controllers/WeightController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * This is the controller that holds the REST endpoints that handle getting,
+ * creating, updating, and deleting Weights.
+ * Spring will automatically convert all  the ResponseEntity and List results
+ * to JSON
+ */
 @SuppressWarnings ( { "unchecked", "rawtypes" } )
 @RestController
 public class WeightController extends APIController {
@@ -32,7 +38,7 @@ public class WeightController extends APIController {
     }
 
     @GetMapping( BASE_PATH + "/weights/{date}" )
-    public ResponseEntity getWeight(@PathVariable("date") final String date) {
+    public ResponseEntity getWeight( @PathVariable("date") final String date ) {
         final Weight w = service.findByDate( date );
 
         return null == w
@@ -54,7 +60,7 @@ public class WeightController extends APIController {
     public ResponseEntity addWeight ( @RequestBody final Weight weight ) {
         if ( null != service.findByDate( weight.getDate() ) ) {
             return new ResponseEntity(
-                    errorResponse( "Ingredient with the name " + weight.getDate() + " already exists" ),
+                    errorResponse( "Weight with the date " + weight.getDate() + " already exists" ),
                     HttpStatus.CONFLICT );
         }
         service.save( weight );
@@ -71,7 +77,7 @@ public class WeightController extends APIController {
      * @param date
      *            the date of the weight
      * @param weight
-     *            the ingredient to be saved
+     *            the weight to be saved
      * @return ResponseEntity indicating success if the Weight could be
      *         saved to the database, or an error if it could not be
      */
