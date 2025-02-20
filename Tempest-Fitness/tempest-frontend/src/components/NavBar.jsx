b25613480b6b1ebbe045a77847391c58d87c52bf
Harsh <harshpxv@gmail.com>
2025-02-20 14:06:48 -0500

    Removed redundant import

--- Tempest-Fitness/tempest-frontend/src/components/NavBar.jsx
+++ Tempest-Fitness/tempest-frontend/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 import stormWeather from "../assets/storm_weather.png";
--- Tempest-Fitness/tempest-frontend/src/components/Weights.jsx
+++ Tempest-Fitness/tempest-frontend/src/components/Weights.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import weightService from "../services/weightService";
 import "../styles/index.css";
 
