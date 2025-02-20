9310f08c7128a2b73f52219db628e56b32737078
Harsh <harshpxv@gmail.com>
2025-02-20 14:55:31 -0500

    Removed redundant React import

--- Tempest-Fitness/tempest-frontend/src/components/Workouts.jsx
+++ Tempest-Fitness/tempest-frontend/src/components/Workouts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import workoutService from "../services/workoutService";
 
 const Workouts = () => {
