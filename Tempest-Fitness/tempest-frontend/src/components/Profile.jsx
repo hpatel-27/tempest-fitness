8beae2cd148de2bf24140b1a4aa959dc34f87838
Harsh <harshpxv@gmail.com>
2025-02-23 23:14:29 -0500

    Use the auth context to get the username for display on the profile

--- Tempest-Fitness/tempest-frontend/src/components/Profile.jsx
+++ Tempest-Fitness/tempest-frontend/src/components/Profile.jsx
@@ -1,6 +1,8 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
 const Profile = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { auth } = useContext(AuthContext);
 
   return (
     <div className="container mt-4">
@@ -10,6 +12,11 @@ const Profile = () => {
           <div className="d-flex justify-content-between align-items-center mb-3">
             <h5 className="mb-0">Profile</h5>
           </div>
+          <ul>
+            <li>
+              <strong>Username:</strong> {auth.username}
+            </li>
+          </ul>
         </div>
       </div>
       <h1>Profile</h1>
