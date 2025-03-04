f6ffeddb27ca3e0396f9fc6adb8466b10113c828
Harsh <harshpxv@gmail.com>
2025-03-03 23:34:46 -0500

    Making the function for the frontend service to add a new weight

--- Tempest-Fitness/tempest-frontend/src/services/weightService.js
+++ Tempest-Fitness/tempest-frontend/src/services/weightService.js
@@ -25,14 +25,28 @@ const getWeights = async (auth) => {
   return await response.json();
 };
 
-// Don't really need this if we list out the weights
-// // Gets a specific weight for a given date
-// const getWeight = async (date) => {
-//   const response = await fetch(`${API_URL}/${date}`);
-//   if (!response.ok) {
-//     throw new Error(`Error: ${response.status} ${response.statusText}`);
-//   }
-//   return await response.json();
-// };
+const addWeight = async (auth, weight) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated");
+  }
 
-export default { getWeights };
+  // Encode credentials in Base64 for Basic Authentication
+  const response = await fetch(WEIGHT_API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: auth.basicAuth,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({weight}),
+  });
+
+  // Throw an error if the response is not OK with the status message
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+}
+
+export default { getWeights, addWeight };
