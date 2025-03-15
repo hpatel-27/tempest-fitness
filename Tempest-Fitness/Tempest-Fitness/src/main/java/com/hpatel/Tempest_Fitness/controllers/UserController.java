7c68e3d5c6c121f6ccfb55097800ce9f0505db3d
Harsh <harshpxv@gmail.com>
2025-03-15 01:19:46 -0400

    Allow for Get access to the current authenticated user

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/controllers/UserController.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/controllers/UserController.java
@@ -5,9 +5,10 @@ import com.hpatel.Tempest_Fitness.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings ( { "unchecked", "rawtypes" } )
@@ -16,6 +17,19 @@ public class UserController extends APIController {
 
     @Autowired
     private UserService service;
+
+    /**
+     * Get the current user from Spring security
+     *
+     * @return Currently authenticated User object
+     */
+    @GetMapping( BASE_PATH + "/currentUser" )
+    public ResponseEntity getCurrentUser() {
+        final User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return null == user ? new ResponseEntity( errorResponse( "Error finding current user" ), HttpStatus.NOT_FOUND )
+                : new ResponseEntity( user, HttpStatus.OK );
+    }
 
     /**
      * REST API method to provide delete access to a User model. This is used to
@@ -28,7 +42,7 @@ public class UserController extends APIController {
      *
      */
     @DeleteMapping( BASE_PATH + "/users/{username}" )
-    public ResponseEntity deleteUser ( @PathVariable final String username ) {
+    public ResponseEntity deleteUser( @PathVariable final String username ) {
         final User user = service.findByName( username );
         if ( null == user ) {
             return new ResponseEntity( errorResponse( "No user found with the name " + username ),
@@ -43,15 +57,15 @@ public class UserController extends APIController {
                 HttpStatus.OK ) : new ResponseEntity( errorResponse( "Error deleting user: " + username ), HttpStatus.BAD_REQUEST );
     }
 
-    /**
-     * Edits a User's information (username, password, name, height)
-     *
-     * @param username The username of the user currently
-     * @param user The new User object with the updated info
-     * @return ResponseEntity indicating outcome of the operation
-     */
-    public ResponseEntity editUser(@PathVariable final String username, @RequestBody User user) {
-        // TODO: Allow users to change their info
-        return null;
-    }
+//    /**
+//     * Edits a User's information (username, password, name, height)
+//     *
+//     * @param username The username of the user currently
+//     * @param user The new User object with the updated info
+//     * @return ResponseEntity indicating outcome of the operation
+//     */
+//    public ResponseEntity editUser(@PathVariable final String username, @RequestBody User user) {
+//        // TODO: Allow users to change their info
+//        return null;
+//    }
 }
