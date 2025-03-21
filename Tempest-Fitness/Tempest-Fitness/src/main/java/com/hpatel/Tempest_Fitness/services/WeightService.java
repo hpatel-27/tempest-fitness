18f2ac751e58ea0ad643dff1fabd73e4edac63ac
Harsh <harshpxv@gmail.com>
2025-03-21 16:33:18 -0400

    Removed unused UserRepository

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/services/WeightService.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/services/WeightService.java
@@ -2,7 +2,6 @@ package com.hpatel.Tempest_Fitness.services;
 
 import com.hpatel.Tempest_Fitness.models.User;
 import com.hpatel.Tempest_Fitness.models.Weight;
-import com.hpatel.Tempest_Fitness.repositories.UserRepository;
 import com.hpatel.Tempest_Fitness.repositories.WeightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,9 +21,6 @@ public class WeightService extends CustomService<Weight, Long> {
     /** The instance of the repository */
     @Autowired
     private WeightRepository weightRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     /**
      * Provides access to the repository
