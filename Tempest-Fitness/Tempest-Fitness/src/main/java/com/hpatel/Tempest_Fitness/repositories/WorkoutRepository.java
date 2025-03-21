f49ed1e2a96cd1aec69d8c1f6a1b315b3b09e740
Harsh <harshpxv@gmail.com>
2025-03-21 18:22:14 -0400

    Changed to findByUserAndDate and findByUser

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/repositories/WorkoutRepository.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/repositories/WorkoutRepository.java
@@ -1,8 +1,11 @@
 package com.hpatel.Tempest_Fitness.repositories;
 
+import com.hpatel.Tempest_Fitness.models.User;
 import com.hpatel.Tempest_Fitness.models.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository is used to find the specific Workout models from the database.
@@ -14,8 +17,16 @@ public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     /**
      * Finds a Workout object with the provided date.
      *
+     * @param user User for the workout
      * @param date date of the workout
      * @return Found workout, null if none.
      */
-    Workout findByDate (String date);
+    Workout findByUserAndDate (User user, String date);
+
+    /**
+     * Finds all the Workouts by a User.
+     * @param user User to query for
+     * @return Found workouts list, null if none
+     */
+    List<Workout> findByUser(User user);
 }
--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/services/WorkoutService.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/services/WorkoutService.java
@@ -1,11 +1,15 @@
 package com.hpatel.Tempest_Fitness.services;
 
+import com.hpatel.Tempest_Fitness.models.User;
 import com.hpatel.Tempest_Fitness.models.Workout;
 import com.hpatel.Tempest_Fitness.repositories.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Provides access to the repository and getting Workout models
  * from the database.
@@ -28,11 +32,22 @@ public class WorkoutService extends CustomService<Workout, Long> {
     }
 
     /**
-     * Finds a Workout model by their date
+     * Finds a Workout model by their date and associated User
+     *
+     * @param user User to find the workout for
      * @param date The date to use to find a Workout
      * @return The found Workout or null
      */
-    public Workout findByDate (final String date ) {
-        return workoutRepository.findByDate( date );
+    public Workout findByUserAndDate(User user, final String date ) {
+        return workoutRepository.findByUserAndDate(user, date);
+    }
+
+    /**
+     * Finds the workouts for a user
+     * @param user The user to find the workouts for
+     * @return The list of workouts for the user
+     */
+    public List<Workout> findByUser(User user) {
+        return workoutRepository.findByUser(user);
     }
 }
