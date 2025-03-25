d16aa06238f25cbb91faf962e474cef772065dfb
Harsh <harshpxv@gmail.com>
2025-03-25 09:18:16 -0400

    Changed to use the exercise api url to get all the available exercises

--- Tempest-Fitness/tempest-frontend/src/services/exerciseService.js
+++ Tempest-Fitness/tempest-frontend/src/services/exerciseService.js
@@ -1,7 +1,8 @@
-const API_URL = "http://localhost/api/v1/exercises";
+const BASE_API_URL = import.meta.env.VITE_API_URL;
+const EXERCISE_API_URL = BASE_API_URL + "/exercises";
 
 const getExercises = async () => {
-  const response = await fetch(API_URL);
+  const response = await fetch(EXERCISE_API_URL);
   if (!response.ok) {
     throw new Error(`Error: ${response.status} ${response.statusText}`);
   }
