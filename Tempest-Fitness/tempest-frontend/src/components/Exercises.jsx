0ac8b8a625be15b7add7e0a2af4579e759d4bd03
Harsh <harshpxv@gmail.com>
2025-03-26 17:21:37 -0400

    The auth needed to be passed to the service method to avoid a 401 error

--- Tempest-Fitness/tempest-frontend/src/components/Exercises.jsx
+++ Tempest-Fitness/tempest-frontend/src/components/Exercises.jsx
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import exerciseService from "../services/exerciseService";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Exercises = () => {
   const [exercises, setExercises] = useState([]);
+  const { auth } = useContext(AuthContext);
 
   useEffect(() => {
-    exerciseService.getExercises().then((data) => setExercises(data));
-  }, []);
+    exerciseService.getExercises(auth).then((data) => {
+      console.log("Fetched exercises: ", data);
+      setExercises(data);
+    });
+  }, [auth]);
 
   return (
     <div>
       <h2>Exercises</h2>
       <ul>
         {exercises.map((exercise, index) => (
-          <li key={index}>
-            {exercise.name} - {exercise.description}
-          </li>
+          <li key={index}>{exercise.name}</li>
         ))}
       </ul>
     </div>
