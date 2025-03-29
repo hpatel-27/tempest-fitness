530c062db25a0fd6046c7a44256e78972cd8133b
Harsh <harshpxv@gmail.com>
2025-03-28 20:01:52 -0400

    Added the method to get all the workouts for the authenticated user

--- Tempest-Fitness/tempest-frontend/src/services/workoutService.js
+++ Tempest-Fitness/tempest-frontend/src/services/workoutService.js
@@ -1,11 +1,26 @@
-const API_URL = "http://localhost/api/v1/workouts";
+const WORKOUT_API_URL = import.meta.env.VITE_WORKOUT_API_URL;
 
-const getWorkouts = async () => {
-  const response = await fetch(API_URL);
+const getWorkouts = async (auth) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated.");
+  }
+
+  // Encode credentials in Base64 for Basic Authentication and get from the weight api
+  const response = await fetch(WORKOUT_API_URL, {
+    method: "GET",
+    headers: {
+      Authorization: auth.basicAuth,
+      "Content-Type": "application/json",
+    },
+  });
+
+  // Throw an error if the response is not OK with the status message
   if (!response.ok) {
     throw new Error(`Error: ${response.status} ${response.statusText}`);
   }
-  return response.data;
+
+  return await response.json();
 };
 
 export default { getWorkouts };
