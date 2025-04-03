058459c890365f63390c16cb2b4b9eed642a4380
Harsh <harshpxv@gmail.com>
2025-04-03 13:26:04 -0400

    Added a quick check to see if the two objects are equal, before checking more specific things for equality

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/models/UserExercise.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/models/UserExercise.java
@@ -179,6 +179,7 @@ public class UserExercise extends DomainObject {
      */
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserExercise that = (UserExercise) o;
         return Objects.equals(getExercise(), that.getExercise());
