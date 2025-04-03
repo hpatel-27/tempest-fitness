af05e6c793d717d67ca8ef2123f8611de4739d66
Harsh <harshpxv@gmail.com>
2025-04-02 22:50:56 -0400

    Added method to get a workout by the date

--- Tempest-Fitness/tempest-frontend/src/services/workoutService.js
+++ Tempest-Fitness/tempest-frontend/src/services/workoutService.js
@@ -23,4 +23,101 @@ const getWorkouts = async (auth) => {
   return await response.json();
 };
 
-export default { getWorkouts };
+const getWorkoutByDate = async (date, auth) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated.");
+  }
+
+  // Encode credentials in Base64 for Basic Authentication and get from the weight api
+  const response = await fetch(`${WORKOUT_API_URL}/${date}`, {
+    method: "GET",
+    headers: {
+      Authorization: auth.basicAuth,
+      "Content-Type": "application/json",
+    },
+  });
+
+  // Throw an error if the response is not OK with the status message
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+};
+
+const createWorkout = async (workout, auth) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated.");
+  }
+
+  // Encode credentials in Base64 for Basic Authentication and get from the weight api
+  const response = await fetch(WORKOUT_API_URL, {
+    method: "POST",
+    headers: {
+      Authorization: auth.basicAuth,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(workout),
+  });
+
+  // Throw an error if the response is not OK with the status message
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+};
+
+const deleteWorkout = async (auth, workoutDate) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated");
+  }
+
+  // fetch from the weight weight api with the date as a path variable
+  const response = await fetch(`${WORKOUT_API_URL}/${workoutDate}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: auth.basicAuth,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+};
+
+const updateWorkout = async (auth, workoutDate, workout) => {
+  // Check if the user is authenticated
+  if (!auth || !auth.basicAuth) {
+    throw new Error("User is not authenticated");
+  }
+
+  // fetch from the weight weight api with the date as a path variable
+  const response = await fetch(`${WORKOUT_API_URL}/${workoutDate}`, {
+    method: "PUT",
+    headers: {
+      Authorization: auth.basicAuth,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(workout),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
+  }
+
+  return await response.json();
+};
+
+export default {
+  getWorkouts,
+  getWorkoutByDate,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
