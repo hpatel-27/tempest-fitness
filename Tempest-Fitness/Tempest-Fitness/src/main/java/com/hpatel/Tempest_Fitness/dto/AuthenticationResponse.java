88e32d7a87dd69e9a37f46a889cb5089989cc104
Harsh <harshpxv@gmail.com>
2025-04-09 22:18:00 -0400

    Creating an AuthResponse DTO for sending the token

---
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.hpatel.Tempest_Fitness.dto;
+
+public class AuthenticationResponse {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
