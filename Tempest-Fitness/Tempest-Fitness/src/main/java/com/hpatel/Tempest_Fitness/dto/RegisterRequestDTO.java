af2f73564d110369e3d2cfb5ad6600550fcb9232
Harsh <harshpxv@gmail.com>
2025-04-09 22:16:57 -0400

    Making the DTO more readable and added the role field

--- Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/dto/RegisterRequestDTO.java
+++ Tempest-Fitness/Tempest-Fitness/src/main/java/com/hpatel/Tempest_Fitness/dto/RegisterRequestDTO.java
@@ -6,11 +6,28 @@ public class RegisterRequestDTO {
     /** Password provided for the register request */
     private String password;
 
+    private String role;
+
     /** Username getter and setter */
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     /** Password getter and setter */
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
